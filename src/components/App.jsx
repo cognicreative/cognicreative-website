a58f8c894868ee0fa40c71a4427742d41555f17f
greggpatton <gregg.patton@gmail.com>
2017-06-04 20:42:24 -0500

    more work in the animated header & footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Header from './Header'
 import Footer from './Footer'
 import AnimateHeaderFooter from './AnimateHeaderFooter'
 
-import '../assets/stylesheets/base.scss'
+// import '../assets/stylesheets/base.css'
 import img from '../assets/images/Logo_ComingSoon_1024.png'
 
 class App extends Component {
@@ -21,18 +21,8 @@ class App extends Component {
                 mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, ligula. Curabitur quam suscipit consectetuer erat convallis nec, vel aliquam, cras mauris, eget quam, neque condimentum nonummy sem. </p>
             <p>Lorem ipsum dolor sit amet, lorem elementum fusce mauris, dolor vestibulum magna temporibus nec metus, ac commodo urna eros vitae ornare lectus. Orci pellentesque at dolor vitae, massa nonummy sit interdum eget, nec nunc pellentesque wisi
                 mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, ligula. Curabitur quam suscipit consectetuer erat convallis nec, vel aliquam, cras mauris, eget quam, neque condimentum nonummy sem. </p>
-            <p>Lorem ipsum dolor sit amet, lorem elementum fusce mauris, dolor vestibulum magna temporibus nec metus, ac commodo urna eros vitae ornare lectus. Orci pellentesque at dolor vitae, massa nonummy sit interdum eget, nec nunc pellentesque wisi
-                mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, ligula. Curabitur quam suscipit consectetuer erat convallis nec, vel aliquam, cras mauris, eget quam, neque condimentum nonummy sem. </p>
-            <p>Lorem ipsum dolor sit amet, lorem elementum fusce mauris, dolor vestibulum magna temporibus nec metus, ac commodo urna eros vitae ornare lectus. Orci pellentesque at dolor vitae, massa nonummy sit interdum eget, nec nunc pellentesque wisi
-                mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, ligula. Curabitur quam suscipit consectetuer erat convallis nec, vel aliquam, cras mauris, eget quam, neque condimentum nonummy sem. </p>
           </section>
           <section className='section'><a href='#link-here'>Link</a>
-            <p>dolor sit amet, lorem elementum fusce mauris, dolor vestibulum magna temporibus nec metus, ac commodo urna eros vitae ornare lectus. Orci pellentesque at dolor vitae, massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi
-                eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan,
-                at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula mi, velit etiam aliquam quis</p>
-            <p>dolor sit amet, lorem elementum fusce mauris, dolor vestibulum magna temporibus nec metus, ac commodo urna eros vitae ornare lectus. Orci pellentesque at dolor vitae, massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi
-                eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan,
-                at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula mi, velit etiam aliquam quis</p>
             <p>dolor sit amet, lorem elementum fusce mauris, dolor vestibulum magna temporibus nec metus, ac commodo urna eros vitae ornare lectus. Orci pellentesque at dolor vitae, massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi
                 eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan,
                 at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula mi, velit etiam aliquam quis</p>
@@ -47,20 +37,8 @@ class App extends Component {
             <p>Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi
                 nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula mi, velit etiam aliquam quis commodo. Turpis sit tortor ornare wisi. Rhoncus dolor, vitae doloremque purus aliquet felis nec, nunc ullamcorper, vel dictum placerat
                 lacinia urna eget pharetra, egestas a sit ipsum lacus cum arcu. Elementum sed ligula eget, non nullam vestibulum nisl sem, lectus parturient, pellentesque tortor massa convallis fermentum ligula. </p>
-            <p>Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi
-                nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula mi, velit etiam aliquam quis commodo. Turpis sit tortor ornare wisi. Rhoncus dolor, vitae doloremque purus aliquet felis nec, nunc ullamcorper, vel dictum placerat
-                lacinia urna eget pharetra, egestas a sit ipsum lacus cum arcu. Elementum sed ligula eget, non nullam vestibulum nisl sem, lectus parturient, pellentesque tortor massa convallis fermentum ligula. </p>
-            <p>Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi
-                nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula mi, velit etiam aliquam quis commodo. Turpis sit tortor ornare wisi. Rhoncus dolor, vitae doloremque purus aliquet felis nec, nunc ullamcorper, vel dictum placerat
-                lacinia urna eget pharetra, egestas a sit ipsum lacus cum arcu. Elementum sed ligula eget, non nullam vestibulum nisl sem, lectus parturient, pellentesque tortor massa convallis fermentum ligula. </p>
           </section>
           <section className='section'>
-            <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
-                et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
-                mi, velit etiam aliquam quis commodo. </p>
-            <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
-                et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
-                mi, velit etiam aliquam quis commodo. </p>
             <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
                 et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
                 mi, velit etiam aliquam quis commodo. </p>
@@ -75,20 +53,8 @@ class App extends Component {
             <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
                 et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
                 mi, velit etiam aliquam quis commodo. </p>
-            <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
-                et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
-                mi, velit etiam aliquam quis commodo. </p>
-            <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
-                et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
-                mi, velit etiam aliquam quis commodo. </p>
           </section>
           <section className='section'>
-            <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
-                et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
-                mi, velit etiam aliquam quis commodo. </p>
-            <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
-                et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
-                mi, velit etiam aliquam quis commodo. </p>
             <p>massa nonummy sit interdum eget, nec nunc pellentesque wisi mauris wisi eget. Turpis accusamus ultrices rutrum tempor lorem, rutrum dis leo aenean volutpat proin. Diam nullam diam id nostra lectus, nulla dapibus. Scelerisque felis quisque
                 et ipsum feugiat, sollicitudin ullamcorper lacus egestas est accumsan, at blanditiis mi, semper ligula. Est eu eu curabitur tellus turpis, quia quam. Mauris morbi nec, ultrices ornare, elit egestas ut euismod orci adipiscing, velit ligula
                 mi, velit etiam aliquam quis commodo. </p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/lib/Navbar'
+import Nav from 'react-bootstrap/lib/Nav'
+import NavItem from 'react-bootstrap/lib/NavItem'
+import NavDropdown from 'react-bootstrap/lib/NavDropdown'
+import MenuItem from 'react-bootstrap/lib/MenuItem'
+
+import img from '../assets/images/favicon.png'
 
 class Header extends Component {
   render () {
     return (
       <header>
-        <Navbar inverse fixedTop id='header'>
+        <Navbar collapseOnSelect fixedTop id='header'>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href='#'>Animated Header</a>
+              <p id='header-text'><img id='logo-icon' src={img} />CogniCreative</p>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href='#'>Item 1</NavItem>
+              <NavItem eventKey={2} href='#'>Item 2</NavItem>
+              <NavItem eventKey={3} href='#'>Item 3</NavItem>
+              <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.4}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </header>
     )
